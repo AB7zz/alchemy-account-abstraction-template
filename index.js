db4891fb7a880d3046261d597b7c86f59be0cf9d
AB7zz <abhinavcv007@gmail.com>
2024-01-07 16:45:50 +0530

    added polygonMumbai

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ import {
   } from "@alchemy/aa-accounts";
   import { AlchemyProvider } from "@alchemy/aa-alchemy";
   import { LocalAccountSigner } from "@alchemy/aa-core";
-  import { sepolia } from "viem/chains";
+  import defineChain from 'viem'
   
-  const chain = sepolia;
+  const polygonMumbai = /*#__PURE__*/ defineChain({
+    id: 80_001,
+    name: 'Polygon Mumbai',
+    nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+    rpcUrls: {
+      default: {
+        http: ['https://rpc.ankr.com/polygon_mumbai'],
+      },
+      alchemy: {
+        http: ['https://polygon-mumbai.g.alchemy.com/v2']
+      }
+    },
+    blockExplorers: {
+      default: {
+        name: 'PolygonScan',
+        url: 'https://mumbai.polygonscan.com',
+      },
+    },
+    contracts: {
+      multicall3: {
+        address: '0xca11bde05977b3631167028862be2a173976ca11',
+        blockCreated: 25770160,
+      },
+    },
+    testnet: true,
+  })
+
+  const chain = polygonMumbai;
+
+  
   
   // The private key of your EOA that will be the owner of Light Account
   // Our recommendation is to store the private key in an environment variable
