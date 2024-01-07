3b46002f29df266dae69171d86f8d0c5cd46aaaa
AB7zz <abhinavcv007@gmail.com>
2024-01-07 16:44:47 +0530

    added private and other keys

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ import {
   
   // The private key of your EOA that will be the owner of Light Account
   // Our recommendation is to store the private key in an environment variable
-  const PRIVATE_KEY = "0xYourEOAPrivateKey";
+  const PRIVATE_KEY = "0xb22b52cadf5b8a828472b2b3356ce4b7c3ec5092b5ed2a868ed51db3ac620c1c";
   const owner = LocalAccountSigner.privateKeyToAccountSigner(PRIVATE_KEY);
   
   // Create a provider to send user operations from your smart account
   const provider = new AlchemyProvider({
     // get your Alchemy API key at https://dashboard.alchemy.com
-    apiKey: "ALCHEMY_API_KEY",
+    apiKey: "kIbxGNByWiwuTZykryjsDc1HU7PpYhfV",
     chain,
   }).connect(
     (rpcClient) =>
